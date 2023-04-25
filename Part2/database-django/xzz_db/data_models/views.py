bbc97ecda1bc169a543e25d5500b99c499141045
Maynic <29720007+Maynic@users.noreply.github.com>
2023-04-24 22:35:45 -0400

    set rest api

--- Part2/database-django/xzz_db/data_models/views.py
+++ Part2/database-django/xzz_db/data_models/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
-# Create your views here.
+from .models import xzz_visitor
+from .serializers import *
+
+@api_view(['GET', 'POST'])
+def visitor_list(request):
+    if request.method == 'GET':
+        data = xzz_visitor.objects.all()
+
+        serializer = VisitorSerializer(data, context={'request': request}, many=True)
+
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = VisitorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT', 'DELETE'])
+def visitor_detail(request, pk):
+    try:
+        visitor = xzz_visitor.objects.get(pk=pk)
+    except xzz_visitor.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = VisitorSerializer(visitor, data=request.data,context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        visitor.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
