280ad5168b75229318ca07e01526ccd7b0202c95
Maynic <29720007+Maynic@users.noreply.github.com>
2023-04-24 20:42:50 -0400

    clean up

--- Part2/database-django/xzz_db/data_models/migrations/0002_visitor.py
+++ Part2/database-django/xzz_db/data_models/migrations/0002_visitor.py
@@ -4,10 +4,10 @@ from django.db import migrations
 
 def create_data(apps, schema_editor):
     Visitor = apps.get_model('data_models', 'xzz_visitor')
-    # Visitor(name="Joe Silver", email="joe@email.com", document="22342342", phone="00000000").save()
-    Visitor(1,'John Smith', 'john.smith@example.com', '1975-06-30', 5551234567,  '123 Main St', 'Anytown', 'CA', 12345,'IN' ).save()
-  
-# (visitor_id, visitor_name, email, birth_date,phone, address, city, state, zip,  visitor_type )
+    Visitor(1, 'John Smith', 'john.smith@example.com', '1975-06-30', 5551234567,  '123 Main St', 'Anytown', 'CA', 12345, 'IN' ).save()
+    # (visitor_id, visitor_name, email, birth_date, phone, address, city, state, zip, visitor_type)
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
--- Part2/database-django/xzz_db/data_models/models.py
+++ Part2/database-django/xzz_db/data_models/models.py
@@ -15,12 +15,6 @@ def validate_zip(value):
                 params={"value": value},
             )
     
-# class Student(models.Model):
-#     name = models.CharField("Name", max_length=240)
-#     email = models.EmailField()
-#     document = models.CharField("Document", max_length=20)
-#     phone = models.CharField(max_length=20)
-#     registrationDate = models.DateField("Registration Date", auto_now_add=True)
 
 #     def __str__(self):
 #         return self.name
